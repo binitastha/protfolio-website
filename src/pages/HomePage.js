84d86f13d5340adc46ac9ccdc226294eb0785278
Binita Shrestha <contactbinitashrestha@gmail.com>
2025-04-25 17:04:57 +0545

    button hover and about us link update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import girlImage from "../images/girl.png";
+import { Link } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
 
 const HomePage = () => (
@@ -20,7 +21,7 @@ const HomePage = () => (
               <br /> <span className="designation">UI Developer</span>
             </h1>
             <p className="mb-4">
-            Hi, I’m Binita Shrestha, a UI developer passionate about creating user-friendly interfaces. With 3 years of experience in web development, I’ve gained the skills needed to deliver successful projects. I enjoy every step of the process, from brainstorming and collaboration to executing the final product. Feel free to explore my work and get in touch!
+              Hi, I’m Binita Shrestha, a UI developer passionate about creating user-friendly interfaces. With 3 years of experience in web development, I’ve gained the skills needed to deliver successful projects. I enjoy every step of the process, from brainstorming and collaboration to executing the final product. Feel free to explore my work and get in touch!
             </p>
             <div class="w-100">
               <div class="about-client-box info-box mb-5">
@@ -40,13 +41,13 @@ const HomePage = () => (
                   </div>
                 </div>
               </div>
-              <a className="custom-btn" href="/about">
+
+              <Link to="/about" className="custom-btn">
                 <span className="text-uppercase">More about me</span>
                 <div className="button-warning icon-box my-0">
                   <FaArrowRight size={14} />
-                  
                 </div>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
