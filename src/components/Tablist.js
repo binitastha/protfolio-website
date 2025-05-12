0268a45313d807becf76156be9be238ed2a817f5
Binita Shrestha <contactbinitashrestha@gmail.com>
2025-05-12 23:17:13 +0545

    changed button css

--- src/components/Tablist.js
+++ src/components/Tablist.js
@@ -5,19 +5,19 @@ import { BsHouse, BsPerson, BsPersonWorkspace, BsChat } from 'react-icons/bs';
 function TabList() {
   return (
     <div className="tab-list d-flex flex-lg-column justify-content-center align-items-end gap-4 gap-lg-2">
-      <Link to="/" className="button-secondary">
+      <Link to="/" className="button-secondary" title='Home'>
         <BsHouse size={16} />
       </Link>
 
-      <Link to="/about" className="button-secondary">
+      <Link to="/about" className="button-secondary" title='About'>
         <BsPerson size={16} />
       </Link>
 
-      <Link to="/projects" className="button-secondary">
+      <Link to="/projects" className="button-secondary" title='Protfolio'>
         <BsPersonWorkspace size={16} />
       </Link>
 
-      <Link to="/contact" className="button-secondary">
+      <Link to="/contact" className="button-secondary" title='Contact'>
         <BsChat size={16} />
       </Link>
     </div>
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,58 @@
 import React from "react";
+// import about from "../images/about.png";
 import coming from "../images/coming-so.png";
 
+
+// const AboutPage = () => (
+
+//   <div className="main-section min-vh-100">
+//     <div className="container homepage">
+//       <div className="row align-items-center justify-content-center">
+//         <div className="col-lg-4 img-desktop-bg p-4 shadow d-none d-lg-block">
+//           <img src={about} alt="Profile" className="w-50" />
+//         </div>
+//         <div className="col-lg-7 offset-lg-4 px-lg-4">
+//           <div className="timeline">
+//             <div className="timeline-item">
+//               <div className="timeline-date">Jan 2023</div>
+//               <div className="timeline-content">
+//                 <h3>Project Kickoff</h3>
+//                 <p>Started development of our new platform with a team of 5 engineers.</p>
+//               </div>
+//             </div>
+
+//             <div className="timeline-item">
+//               <div className="timeline-date">Mar 2023</div>
+//               <div className="timeline-content">
+//                 <h3>Alpha Release</h3>
+//                 <p>First version released to internal testers with core functionality.</p>
+//               </div>
+//             </div>
+
+//             <div className="timeline-item">
+//               <div className="timeline-date">Jun 2023</div>
+//               <div className="timeline-content">
+//                 <h3>Public Beta</h3>
+//                 <p>Opened platform to first 1000 users with feedback system in place.</p>
+//               </div>
+//             </div>
+
+//             <div className="timeline-item">
+//               <div className="timeline-date">Sep 2023</div>
+//               <div className="timeline-content">
+//                 <h3>Version 1.0</h3>
+//                 <p>Official launch with all planned features and marketing campaign.</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
 const AboutPage = () => (
-  <div class="container d-flex align-items-center justify-content-center min-vh-100 inner-pages">
+  <div class="container d-flex align-items-center justify-content-center min-vh-100">
                  <img src={coming} alt="Profile" className="w-50" />
 
   </div>
